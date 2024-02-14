fea41d95ce4c054db451087b0145d05f6acaf3dc
Kade <26305836+Kade-github@users.noreply.github.com>
2024-02-14 13:43:18 -0800

    format main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 #include "Shader.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
@@ -12,26 +12,24 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 int main()
 {
     if (!glfwInit())
-	{
-		std::cout << "Failed to setup GLFW" << std::endl;
-		exit(EXIT_FAILURE);
-	}
+    {
+        std::cout << "Failed to setup GLFW" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-    glfwSetErrorCallback([](int error, const char* description)
-	{
-		std::cout << "GLFW Error - " << error << " " << description << std::endl;
-	});
+    glfwSetErrorCallback([](int error, const char *description)
+                         { std::cout << "GLFW Error - " << error << " " << description << std::endl; });
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-	glfwWindowHint(GLFW_DOUBLEBUFFER, GL_TRUE);
-	glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+    glfwWindowHint(GLFW_DOUBLEBUFFER, GL_TRUE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
 
     /* Create render thread */
-    GLFWwindow* window = NULL;
+    GLFWwindow *window = NULL;
 
     /* Create a windowed mode window */
     window = glfwCreateWindow(1280, 720, "3D Test", NULL, NULL);
@@ -48,29 +46,28 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    
+
     glViewport(0, 0, 1280, 720);
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     std::cout << "GLFW created." << std::endl;
 
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    }; 
+        0.0f, 0.5f, 0.0f};
 
-    Shader* s = new Shader();
-    s->LoadShader("Shaders/VertexShader.txt","Shaders/FragmentShader.txt");
+    Shader *s = new Shader();
+    s->LoadShader("Shaders/VertexShader.txt", "Shaders/FragmentShader.txt");
 
     unsigned int VBO;
     glGenBuffers(1, &VBO);
 
     unsigned int VAO;
-    glGenVertexArrays(1, &VAO);  
+    glGenVertexArrays(1, &VAO);
 
-    while(!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -79,8 +76,8 @@ int main()
 
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0); 
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+        glEnableVertexAttribArray(0);
 
         s->Bind();
 
@@ -89,7 +86,7 @@ int main()
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
-        glfwPollEvents();    
+        glfwPollEvents();
     }
 
     glfwTerminate();
